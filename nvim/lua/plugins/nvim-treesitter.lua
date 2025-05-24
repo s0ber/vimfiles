6f460119d4e13258cfc19636e19f0006c5df0044
Sergei Shishkalov <sergeyshishkalov@gmail.com>
2025-05-24 17:42:29 +0300

    Add ruby and slim highlighting

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup()
   require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'javascript', 'typescript', 'tsx', 'json', 'html', 'css', 'scss', 'lua', 'vim', 'query' },
+    ensure_installed = { 'javascript', 'typescript', 'tsx', 'json', 'html', 'css', 'scss', 'lua', 'vim', 'query', 'ruby', 'slim' },
     highlight = {
       enable = true,
       vim.api.nvim_set_hl(0, "@variable.builtin.javascript", { link = "@variable" })
